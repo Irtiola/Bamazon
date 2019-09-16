02d9cf87dbe5c4922198cc06c2db4f3be27d1619
Irina K <irtiola@gmail.com>
2019-09-15 19:35:54 -0700

    all works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,11 @@
-var mysql = require('mysql');
 
+// require("dotenv").config();
 // Load the NPM Package inquirer
 var inquirer = require("inquirer");
-// mysql -u root -p
+const keys = require("./keys.js");
+var mysql = require('mysql');
+var connection = mysql.createConnection(keys.data);
+//mysql -u root -p
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -12,9 +15,10 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 allProducts();
+
 //function that will show all products
 function allProducts() {
-    connection.query('SELECT * FROM Products', function (error, results, fields) {
+    connection.query('SELECT * FROM Products', function (error, results) {
         if (error) throw error;
         console.log('--------------------------')
         console.log('--------ALLProducts-------')
@@ -24,34 +28,38 @@ function allProducts() {
     });
 
 };
-//function that will update quantity for products after they were bought
-function updateQuantity(iid, num_units) {
-    connection.query('UPDATE Products SET stock_quantity = stock_quantity -? WHERE item_id = ?', [num_units, iid], function (error, results, fields) {
-        allProducts();
-        userChoicePrompt();
-    });
-};
-
 //function that will compare user input with stock quantity  and based on result will proceed accordingly
 function checkQuantity(num_units, iid) {
-    connection.query('SELECT * FROM Products WHERE item_id = ?', [iid], function (error, results, fields) {
-        if (error) throw error;
-        if (parseInt(results[0].stock_quantity >= num_units)) {
+    connection.query('SELECT * FROM Products WHERE item_id = ?', [iid], function (error, results) {
+        // if (error) throw error;
+        console.log(results[0].stock_quantity);
+        if (parseInt(results[0].stock_quantity) >= num_units) {
             console.log('-----------------------------')
-            console.log("Thank you for your order! Total cost is: " + num_units * results[0].price + "$");
+            console.log("*************************************")
+            console.log("Thank you for your order! ");
+            console.log("Order details: " + results[0].product_name + "*" + results[0].price);
+            console.log("TOTAL COST IS: " + num_units * results[0].price + "$");
             console.log('-----------------------------')
+            console.log("*************************************")
             updateQuantity(num_units, iid);
         } else {
             console.log("Insufficient Quantity!")
-            console.log('******************************');
-            allProducts();
-            userChoicePrompt();
+            console.log('*************************************');
+            // allProducts();
+            // userChoicePrompt();
 
         };
     });
 
 };
-
+//function that will update quantity for products after they were bought
+function updateQuantity(num_units, iid) {
+    connection.query('UPDATE Products SET stock_quantity = stock_quantity -? WHERE item_id = ?', [num_units, iid], function (error, results, fields) {
+        // allProducts();
+        // userChoicePrompt();
+        startOverPromt();
+    });
+};
 //function that will show questions to user
 function userChoicePrompt() {
     inquirer
@@ -75,3 +83,22 @@ function userChoicePrompt() {
 
 };
 
+function startOverPromt() {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Do you want to buy something else?",
+                name: "conf",
+                default: true
+            },
+        ])
+        .then(nextMove => {
+            if (nextMove.conf) {
+                allProducts();
+            } else {
+                console.log("Thank you for choosing Bamazon!")
+
+            }
+        });
+}
